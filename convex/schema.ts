32c9ca23282906bd52c78600b8e5c5166824f1dd
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-12-16 19:28:25 +0545

    initialized convex with clerk

---
+++ convex/schema.ts
@@ -0,0 +1,22 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        userId: v.string(),
+        name: v.string(),
+        email: v.string(),
+        isPro: v.boolean(),
+        proSince: v.optional(v.number()),
+        lemonSqueezyCustomerId: v.optional(v.string()),
+        lemonSqueezyOrderId: v.optional(v.string()),
+    }).index("by_user_id", ["userId"]),
+
+    codeExecution: defineTable({
+        userId: v.string(),
+        language: v.string(),
+        code: v.string(),
+        output: v.optional(v.string()),
+        error: v.optional(v.string())
+    })
+})
