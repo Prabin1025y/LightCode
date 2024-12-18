78c573c89ae52ba1b897ff8e7d5cf7f5af94893f
Prabin1025y <acharyaprabin1025y@gmail.com>
2024-12-18 20:40:06 +0545

    Implement theme selection functionality with dropdown and click outside detection

--- src/app/(root)/_components/ThemeSelector.tsx
+++ src/app/(root)/_components/ThemeSelector.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+'use client';
+import { useCodeEditorStore } from '@/Store/UseCodeEditorStore';
+import React, { useEffect, useRef, useState } from 'react'
+import { THEMES } from '../_constants';
 
 const ThemeSelector = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { theme, setTheme } = useCodeEditorStore();
+  const currentTheme = THEMES.find(item => theme === item.id);
+  const dropDownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropDownRef.current && !dropDownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [])
+
+
   return (
     <div>ThemeSelector</div>
   )
